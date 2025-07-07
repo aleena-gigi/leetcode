dd7ba9a90da042fdf44c3c4ad7e2e11243240d03
aleena-gigi <aleenagigi16@gmail.com>
2025-07-07 11:02:13 +0530

    added space and time complexities

--- twosum.java
+++ twosum.java
@@ -1,11 +1,15 @@
 import java.util.HashMap;
 
 public class twosum {
+    /*
+     * Time complexity: O(n)
+     * Space complexity: O(n)
+     */
     public int[] twoSum(int[] nums, int target) {
-       HashMap<Integer, Integer> num = new HashMap<>();
-       for(int i = 0; i< nums.length; i++) {
+       HashMap<Integer, Integer> num = new HashMap<>(); // worst case store all n elements, space complexity: O(n)
+       for(int i = 0; i< nums.length; i++) {  // Time complexity: O(n)
             int diff = target - nums[i];
-            if (num.containsKey(diff)) {
+            if (num.containsKey(diff)) { // O(1) average time complexity for HashMap lookup
                 return new int[] {num.get(diff), i};
             }
             num.put(nums[i], i);
